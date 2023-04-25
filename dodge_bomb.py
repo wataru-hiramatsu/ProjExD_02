a6be2e0c45cc41103b853636cf4ae168b13f620f
shadero <shadero.vrc@gmail.com>
2023-04-25 16:15:14 +0900

    追加機能3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -30,6 +30,8 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     kk_img_flip = pg.transform.flip(kk_img, True, False)
+    kk_gameover_img = pg.image.load("ex02/fig/6.png")
+    kk_gameover_img = pg.transform.rotozoom(kk_gameover_img, -90, 2.0)
     kk_rect = kk_img.get_rect()
     kk_rect.center = (900, 400)
     # キー毎の移動量
@@ -67,13 +69,25 @@ def main():
         pg.draw.circle(img, (255, 0, 0), (10 * r, 10 * r), 10 * r)
         img.set_colorkey((0, 0, 0))
         bomb_imgs.append(img)
+    is_gameover = False
 
     tmr = 0
+    gameover_tmr = 0
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return 0
+        tmr += 1
+        screen.blit(bg_img, [0, 0])
+        if is_gameover:
+            screen.blit(kk_gameover_img, kk_rect)
+            if tmr - gameover_tmr >= 3000:
+                return
+            pg.display.update()
+            clock.tick(1000)
+            continue
+
         key_lst = pg.key.get_pressed()
         kk_velo = [0, 0]
         for key in kk_move_assign.keys():
@@ -89,10 +103,6 @@ def main():
         if (not bound[0]) or (not bound[1]):
             kk_rect.center = pos
 
-        tmr += 1
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, kk_rect)
-
         bomb_rect.move_ip(bomb_velocity)
         bound = check_bound(screen, bomb_rect)
         bomb_velocity[0] = sign(bomb_velocity[0]) * bomb_accs[min(tmr // 1000, 9)]
@@ -105,7 +115,11 @@ def main():
         screen.blit(bomb_imgs[min(tmr // 1000, 9)], bomb_rect)
 
         if kk_rect.colliderect(bomb_rect):
-            return
+            screen.blit(kk_gameover_img, kk_rect)
+            is_gameover = True
+            gameover_tmr = tmr
+        else:
+            screen.blit(kk_img, kk_rect)
 
         pg.display.update()
         clock.tick(1000)
